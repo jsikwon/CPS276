b80924632fe1b28c61071c5bd5eb9095c1289a19
Jun Sik Won <jsikwon@gmail.com>
2022-10-10 11:04:46 -0400

    updates

--- assignments/assignment3/index.php
+++ assignments/assignment3/index.php
@@ -1,4 +1,13 @@
-<?php
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calculator Demo</title>
+  </head>
+  <body>
+    <?php
         require_once "Calculator.php";
         $Calculator = new Calculator();
         echo $Calculator->calc("/", 10, 0);
@@ -9,14 +18,5 @@
         echo $Calculator->calc("*", 10);
         echo $Calculator->calc(10);
     ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Calculator Demo</title>
-  </head>
-  <body>
   </body>
 </html>
