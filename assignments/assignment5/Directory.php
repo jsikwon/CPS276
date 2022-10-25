9898548470dad539478b7fcb591fe16142917b89
Jun Sik Won <jsikwon@gmail.com>
2022-10-24 23:06:55 -0400

    updates

--- assignments/assignment5/Directory.php
+++ assignments/assignment5/Directory.php
@@ -1,7 +1,7 @@
 <?php
 
     $msg = "";
-class createDirectory() {
+class createDirectory {
     public function addFile() { 
         if(isset($_POST["submitButton"]))
         
@@ -15,7 +15,9 @@ class createDirectory() {
                 fclose($file);
 
         if($success) {
-            $msg = "File and directory where created\n <p><a href="https://russet-v8.wccnet.edu/~jwon1/CPS276/assignments/assignment5/directories/"$folderName"/readme.txt">Path where file is located</a></p>";
+            $link = <p><a href="https://russet-v8.wccnet.edu/~jwon1/CPS276/assignments/assignment5/directories/"$folderName"/readme.txt">Path where file is located</a></p>;
+            $msg = "File and directory where created\n".$link
+             
 
         } else {
             $msg = "A directory already exists with that name";
